62caebe7c943e3d0b0de2e971d9ab79fb83c3cae
aprezvykh <aprezvykh@yandex.ru>
2018-11-02 13:04:44 +0300

    data add

---
+++ install.R
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+print ("This script will automatically install packages!")
+
+need.pack <- c("rtracklayer","stringr", "parallel", "plyr", "dplyr", "Biostrings")
+inst <- installed.packages()
+
+for (f in need.pack){
+  g <- grep(f, inst)
+  if(length(g)<1){
+    print(paste(f, "is not found in you system! Installing..."))
+    install.packages(f)
+  } else {
+    print(paste(f, "is found!"))
+  }
+  
+}
+
+
+print("All R packages installed!")
